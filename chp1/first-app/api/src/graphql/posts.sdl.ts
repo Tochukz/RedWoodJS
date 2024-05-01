51c2789b7eed52c8e17927b659aa80bd2100c5e3
Tochukwu Nwachukwu <truetochukz@gmail.com>
2024-05-01 14:31:40 +0200

    Added Scaffolding for Post model

---
+++ chp1/first-app/api/src/graphql/posts.sdl.ts
@@ -0,0 +1,29 @@
+export const schema = gql`
+  type Post {
+    id: Int!
+    title: String!
+    author: String!
+    createdAt: DateTime!
+  }
+
+  type Query {
+    posts: [Post!]! @requireAuth
+    post(id: Int!): Post @requireAuth
+  }
+
+  input CreatePostInput {
+    title: String!
+    author: String!
+  }
+
+  input UpdatePostInput {
+    title: String
+    author: String
+  }
+
+  type Mutation {
+    createPost(input: CreatePostInput!): Post! @requireAuth
+    updatePost(id: Int!, input: UpdatePostInput!): Post! @requireAuth
+    deletePost(id: Int!): Post! @requireAuth
+  }
+`
